28bbd79e67f896b14b73031d88cd60b2b49a50d9
GraydonE <124379361+GraydonE@users.noreply.github.com>
2023-06-01 14:08:43 +0000

    added game.js

---
+++ js/game.js
@@ -0,0 +1,11 @@
+/* global Phaser */
+
+const config = {
+  type: Phaser. AUTO, 
+  width: 1029,
+  height: 1080, 
+  backgroundColor: 0x5f6e7a
+}
+
+const game + new Phaser.Game(config)
+cosole.log(game)
