abf9c768ebcb9476e421fa6d194b01e54036e889
GraydonE <124379361+GraydonE@users.noreply.github.com>
2023-06-01 14:28:39 +0000

    Added favicon and Commands to our files

--- js/game.js
+++ js/game.js
@@ -1,11 +1,32 @@
 /* global Phaser */
 
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Modified by: Graydon Ezzeddin
+// Created on: June 2023
+// This is the Phaser3 configuration file
+
+// Game Scene
 const config = {
   type: Phaser. AUTO, 
   width: 1029,
   height: 1080, 
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+// Set Background Colour
+  backgroundColor: 0x5f6e7a,
+    scale: {
+    mode: Phaser.Scale.FIT,
+    // place it in middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
-const game + new Phaser.Game(config)
+
+const game = new Phaser.Game(config) 
 cosole.log(game)
