19faacef65d43cecf7e972b4a535a3fa355a0078
GraydonE <124379361+GraydonE@users.noreply.github.com>
2023-06-11 02:22:29 +0000

    13 working

--- js/gameScene.js
+++ js/gameScene.js
@@ -8,75 +8,105 @@
 // This is the Game Scene
 
 class GameScene extends Phaser.Scene {
-
   // create a polar bear
-  createPolar () {
-    const polarXLocation = Math.floor(Math.random() * 1920) + 1 // get a number between 1 and 1920
-    const aPolar = this.physics.add.sprite(polarXLocation , 100, 'polar')
-    aPolar.body.velocity.y = 200 //7.24
+  createPolar() {
+    const polarXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
+    let polarXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50
+    polarXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
+    const aPolar = this.physics.add.sprite(polarXLocation, -100, 'polar')
+    aPolar.body.velocity.y = 200
+    aPolar.body.velocity.x = polarXVelocity
     this.polarGroup.add(aPolar)
   }
 
-  constructor () {
+  constructor() {
     super({ key: 'gameScene' })
 
-    this.background= null
-    this.ship= null
+    this.background = null
+    this.ship = null
     this.fireMissile = false
   }
 
-  init (data) {
+  init(data) {
     this.cameras.main.setBackgroundColor('#ffffff')
   }
 
-  preload () { 
+  preload() {
     console.log('Game Scene')
     //images
     this.load.image('articBackground', './assets/articBackground.png')
     this.load.image('ship', './assets/seal.png')
-    this.load.image('missile', './assets/laser.png') 
+    this.load.image('missile', './assets/laser.png')
     this.load.image('polar', './assets/polarbear.png')
-    // sounds 
+    // sounds
     this.load.audio('laser', './sounds/lasersound.mp3')
+    this.load.audio('polardeathsound', './sounds/polarbeardeath.mp3')
   }
 
-  create (data) {
+  create(data) {
     // add background
     this.background = this.add.image(0, 0, 'articBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
-    // add seal 
+    // add seal
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship').setScale(0.5)
-  
+
     // Create group for lasers
-  this.missileGroup = this.physics.add.group()
+    this.missileGroup = this.physics.add.group()
 
     // Create group for Polar bears
-  this.polarGroup = this.add.group()
+    this.polarGroup = this.add.group()
     // Function to create polar bears
-  this.createPolar()
-  }
+    this.createPolar()
 
+    // Collisions between missiles and polar bears
+    this.physics.add.collider(this.missileGroup, this.polarGroup, function (missileCollide, polarCollide) {
+      polarCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play('polardeathsound')
+      this.createPolar()
+      this.createPolar()
+    }.bind(this))
+  }
+  
   update (time, delta) {
     //Add controls
     const keyLeftObj = this.input.keyboard.addKey('LEFT')
     const keyRightObj = this.input.keyboard.addKey('RIGHT')
+    const keyAObj = this.input.keyboard.addKey('A');
+    const keyDObj = this.input.keyboard.addKey('D');
     const keySpaceObj = this.input.keyboard.addKey('SPACE')
     
-    // Seal can go left
+    // Seal can go left using the arrow key
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
       if (this.ship.x < 0) { // Seal wraps back around
         this.ship.x = 1920
       }
     }
-    // Seal can move right
+
+    // Seal can go left using a
+    if (keyAObj.isDown === true) {
+      this.ship.x -= 15;
+      if (this.ship.x < 0) { // Seal wraps back around
+        this.ship.x = 1920;
+      }
+    }
+    // Seal can move right using the arrow key 
     if (keyRightObj.isDown === true) {
       this.ship.x += 15
       if (this.ship.x > 1920) { // Ship wraps back around
         this.ship.x = 0  
       }
     }
-
+  
+    // Seal can move right using the D key
+    if (keyDObj.isDown === true) {
+      this.ship.x += 15;
+      if (this.ship.x > 1920) { // Ship wraps back around
+        this.ship.x = 0;
+      }
+    }
+  
     // Seal can shoot lasers
     if (keySpaceObj.isDown === true) {
       if (this.fireMissile === false) {
@@ -98,7 +128,6 @@ class GameScene extends Phaser.Scene {
       }
     })
   }
-}
-
+} 
 
 export default GameScene
